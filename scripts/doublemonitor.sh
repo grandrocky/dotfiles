917c3a5a9b5f0ae518da0427a301e5ae218375a9
Ander <anderraso@gmail.com>
2015-02-24 03:17:56 +0100

    Actualizado

--- scripts/doublemonitor.sh
+++ scripts/doublemonitor.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-xrandr --output VGA-0 --mode 1440x900 --left-of LVDS 
-xrandr --output LVDS --mode 1366x768          
-nitrogen --restore
+hay_monitor=$(xrandr | grep "VGA-0 disconnected")
+
+if [ -n "$hay_monitor" ]; then
+    echo "No existe el monitor VGA"
+else
+    xrandr --output VGA-0 --mode 1440x900 --left-of LVDS 
+    xrandr --output LVDS --mode 1366x768          
+    xrandr --output LVDS --primary
+    nitrogen --restore
+    echo "Monitor VGA configurado a la izquierda"
+fi
